a4409b35f0f8a5c05f69691a3e0bc25e734c7be6
Fr1x3 <gathufx@gmail.com>
2021-06-10 11:52:52 -0700

    navigation set up

--- app/src/main/java/com/fr1x3/invoicingan/ui/views/Invoices.kt
+++ app/src/main/java/com/fr1x3/invoicingan/ui/views/Invoices.kt
@@ -1,6 +1,21 @@
 package com.fr1x3.invoicingan.ui.views
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.fr1x3.invoicingan.databinding.InvoicesBinding
 
 class Invoices : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding = InvoicesBinding.inflate(inflater)
+        return  binding.root
+    }
 }
--- app/src/main/java/com/fr1x3/invoicingan/ui/views/Profile.kt
+++ app/src/main/java/com/fr1x3/invoicingan/ui/views/Profile.kt
@@ -1,6 +1,21 @@
 package com.fr1x3.invoicingan.ui.views
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.fr1x3.invoicingan.databinding.ProfileBinding
 
 class Profile : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding = ProfileBinding.inflate(inflater)
+        return  binding.root
+    }
 }
